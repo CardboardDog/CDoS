2c2ba5a49e584fafe86e01e0138915c106684732
CardboardDog <109923311+CardboardDog@users.noreply.github.com>
2023-11-03 17:06:39 -0400

    better demo

--- kernel.c
+++ kernel.c
@@ -17,28 +17,38 @@ void boot_kernel(){
     int x_c=0;
     int y_c=0;
     int start_char=cursor_i;
+    uint8 old_key_map[53];
+    uint8 used_chars=0;
     while(1){
         check_keys();
         int read_k;
         for(read_k=0;read_k<53;read_k++){
             if(key_map[read_k]==1){
-                if(read_k==KEY_BACKSPACE){
-                    if(cursor_i>start_char){
-                        move_cursor((cursor_i-(line*80)-1),line);
-                        display_text(" ",WHITE);
-                        move_cursor((cursor_i-(line*80)-1),line);
+                if(old_key_map[read_k]!=key_map[read_k]){
+                    if(read_k==KEY_BACKSPACE){
+                        if(cursor_i>start_char){
+                            move_cursor((cursor_i-(line*80)-1),line);
+                            display_text(" ",WHITE);
+                            move_cursor((cursor_i-(line*80)-1),line);
+                            used_chars--;
+                        }
                     }
-                }else if(read_k == KEY_RETURN){
-                    new_line();
-                    display_text("root.CDoS:> ",WHITE);
-                    set_cursor(cursor_i);
-                    start_char=cursor_i;
-                }else{
-                    display_char(text_map[read_k+(53*key_shift)],RED);
-                    set_cursor(cursor_i);
+                    else if(read_k == KEY_RETURN){
+                        new_line();
+                        display_text("root.CDoS:> ",WHITE);
+                        set_cursor(cursor_i);
+                        start_char=cursor_i;
+                        used_chars=0;
+                    }else{
+                        if(used_chars<67){
+                            display_char(text_map[read_k+(53*key_shift)],RED);
+                            set_cursor(cursor_i);
+                            used_chars++;
+                        }
+                    }
                 }
             }
+            old_key_map[read_k]=key_map[read_k];
         }
-        sleep((uint32)55000);
     }
 }
--- time.c
+++ time.c
@@ -1,4 +1,5 @@
 #include "time.h"
+// todo: fix later
 void sleep(uint32 ms){
     uint32 i_m;
     for(i_m=0;i_m<ms;i_m++){
